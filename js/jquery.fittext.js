d7b70964cb3e318475be86a9bc13cae3355be721
cynthiachan <cynthiac256@gmail.com>
2016-04-27 22:00:01 -0400

    first commit

---
+++ js/jquery.fittext.js
@@ -0,0 +1,187 @@
+<!DOCTYPE HTML>
+<!--
+	Colorized by TEMPLATED
+    templated.co @templatedco
+    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Colorized by TEMPLATED</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--	<link href='http://fonts.googleapis.com/css?family=Questrial' rel='stylesheet' type='text/css'>-->
+		<!--changing font to questrial to roboto -->
+		<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-panels.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel-noscript.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+	</head>
+	<body>
+
+	<!-- Header -->
+		<div id="header">
+			<div id="logo-wrapper">
+				<div class="container">
+
+					<!-- Logo -->
+						<div id="logo">
+							<h1><a href="#">manhattan express</a></h1>
+							<span>discover a different taste</span>
+						</div>
+
+				</div>
+			</div>
+			<div class="container">
+				<!-- Nav -->
+					<nav id="nav">
+						<ul>
+							<li><a href="index.html">Homepage</a></li>
+							<li><a href="left-sidebar.html">search by cuisine</a></li>
+							<li><a href="right-sidebar.html">search by district</a></li>
+							<li><a href="two-sidebars.html">gallery</a></li>
+							<li class="active"><a href="no-sidebar.html">favorites</a></li>
+						</ul>
+					</nav>
+			</div>
+		</div>
+	<!-- Header -->
+
+	<!-- Banner -->
+		<div id="banner">
+			<div class="container">
+			</div>
+		</div>
+	<!-- /Banner -->
+
+	<!-- Main -->
+		<div id="main">
+
+			<!-- Main Content -->
+			<div class="container">
+				<div class="row">
+					<div class="12u">
+						<section>
+							<header>
+								<h2>Creperie </h2>
+							</header>
+							<h2> 135 Ludlow St, New York, NY 10002  </h2>
+							<p>Creperie is one of my favorite eateries in Manhattan New York City.
+								It is located near Delancey street on the F and M train of the subway station.
+								My favorite dish on their menu is the famous crepe (strawberries, bananas, nutella, whipped cream and ice cream).
+								I go there often with my friends, and it is one of our to-go-places. It is a great place to sit down, hang out and
+								 eat desserts. They also serve chilled drinks. Their space is kind of small,
+								 but it also gives off a cozy vibe, in which a few friends can share a simple moment
+								  together.
+
+							</p>
+
+						</section>
+						<section>
+							<header>
+								<h2>Juicy Spot Cafe</h2>
+							</header>
+							<p>Another eatery that I recommend near Delancy Street is called Juicy Spot Cafe.
+								 Down a couple of blocks from Creperie, the Juicy Spot cafe is located specifically at
+								 109 Ludlow St, New York, NY 10002. It has a 4.5 rating on yelp and boasts a diverse menu.
+							   Although it also occupies a small square feet of space, it is crowded with customers at all times of the day.
+							   One of their popular</p>
+						</section>
+						<section>
+							<header>
+								<h2>Soho Thai</h2>
+							</header>
+							<p></p>
+						</section>
+						<section>
+							<header>
+								<h2>Vivi's Bubble Tea</h2>
+							</header>
+							<p></p>
+						</section>
+						<section>
+							<header>
+								<h2>Thái Son</h2>
+							</header>
+							<p></p>
+						</section>
+					</div>
+				</div>
+
+			</div>
+			<!-- /Main Content -->
+
+		</div>
+	<!-- /Main -->
+
+	<!-- Footer -->
+		<div id="footer">
+			<div class="container">
+				<div class="row">
+					<div class="6u">
+						<section>
+							<header>
+								<h2>Elementum facilisis</h2>
+							</header>
+							<a href="#" class="image full"><img src="images/pics05.jpg" alt=""></a>
+							<p>In posuere eleifend odio. Quisque semper augue mattis wisi. Maecenas ligula. Pellentesque viverra vulputate enim. Aliquam erat volutpat. Pellentesque tristique ante ut risus. Quisque dictum. Integer nisl risus, sagittis convallis, rutrum id, elementum.</p>
+						</section>
+					</div>
+					<div id="fbox1" class="3u">
+						<section>
+							<header>
+								<h2>Praesent mattis</h2>
+							</header>
+							<ul class="default">
+								<li class="fa fa-angle-right"><a href="#">Vestibulum luctus venenatis dui</a></li>
+								<li class="fa fa-angle-right"><a href="#">Integer rutrum nisl in mi</a></li>
+								<li class="fa fa-angle-right"><a href="#">Etiam malesuada rutrum enim</a></li>
+								<li class="fa fa-angle-right"><a href="#">Aenean elementum facilisis ligula</a></li>
+								<li class="fa fa-angle-right"><a href="#">Ut tincidunt elit vitae augue</a></li>
+								<li class="fa fa-angle-right"><a href="#">Sed quis odio sagittis leo vehicula</a></li>
+							</ul>
+						</section>
+					</div>
+					<div id="fbox2" class="3u">
+						<section>
+							<header>
+								<h2>Maecenas luctus</h2>
+							</header>
+							<ul class="default">
+								<li class="fa fa-angle-right"><a href="#">Vestibulum luctus venenatis dui</a></li>
+								<li class="fa fa-angle-right"><a href="#">Integer rutrum nisl in mi</a></li>
+								<li class="fa fa-angle-right"><a href="#">Etiam malesuada rutrum enim</a></li>
+								<li class="fa fa-angle-right"><a href="#">Aenean elementum facilisis ligula</a></li>
+								<li class="fa fa-angle-right"><a href="#">Ut tincidunt elit vitae augue</a></li>
+								<li class="fa fa-angle-right"><a href="#">Sed quis odio sagittis leo vehicula</a></li>
+							</ul>
+						</section>
+					</div>
+				</div>
+
+
+			</div>
+		</div>
+	<!-- /Footer -->
+
+	<!-- Copyright -->
+		<div id="copyright">
+			<div class="container">
+				<section>
+				 <p>	&copy; COPYRIGHT 2016 MANHATTAN EXPRESS &bull; Design by TEMPLATED &bull; Terms | Privacy </p>
+				</section>
+			</div>
+		</div>
+
+
+	</body>
+</html>
